af7c16fab79588d2087759bbc7af04fe365b8925
baluR220 <39237892+baluR220@users.noreply.github.com>
2019-04-12 10:26:07 +0700

    Update client.py

--- server/client.py
+++ server/client.py
@@ -3,8 +3,6 @@ from tkinter import *
 
 #server = '109.233.239.229'
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#sock.sendto(('Hello').encode(), (server, 11719))
-print('Connection is on')
 
 master = Tk()
 master.title('TTT_online')
@@ -35,7 +33,6 @@ main_canvas.create_text(200, 140, text='Connect to:', justify=CENTER, font='Aria
 
 conadr = StringVar()
 coninpt = Entry(main_canvas, width=30, textvariable=conadr)
-#coninpt.insert(0, 'server')
 coninpt_window = main_canvas.create_window(200,160, window=coninpt, tag='conn')
 
 is_local = IntVar()
@@ -49,7 +46,92 @@ def change_state():
         coninpt.config(state='normal')
 check_local = Checkbutton(main_canvas, text='Local play', variable=is_local, command=change_state)
 check_local_window = main_canvas.create_window(200, 190, window=check_local, tag='conn')
-        
+
+count = 0
+def draw_field():
+    main_canvas.create_line(150,100,150,400, fill='black')
+    main_canvas.create_line(250,100,250,400, fill='black')
+    main_canvas.create_line(50,200,350,200, fill='black')
+    main_canvas.create_line(50,300,350,300, fill='black')
+    main_canvas.create_text(200,450, text='Press Space to restart', justify=CENTER, font='Arial 14')
+ 
+def draw_cross(x,y):
+    main_canvas.create_polygon(x-10,y,x-40,y-30,x-30,y-40,
+                     x,y-10,x+30,y-40,x+40,y-30,
+                     x+10,y,x+40,y+30,x+30,y+40,
+                     x,y+10,x-30,y+40,x-40,y+30, fill='red',tag='figure')
+    
+def draw_circle(x,y):
+    main_canvas.create_oval(x-40,y-40,x+40,y+40, fill='blue',tag='figure')
+    main_canvas.create_oval(x-30,y-30,x+30,y+30, fill='white',tag='figure')
+
+def draw(event):
+    x,y = event.x, event.y
+    global count, dest
+    if count == 1:
+        draw_cross(x,y)
+        count = 0
+    else:
+        draw_circle(x,y)
+        count = 1
+    sock.sendto((str(x)+'_'+str(y)+str(count)).encode(), dest)
+
+def draw_con(data): 
+    global count
+    count = int(data[-1:])
+    #print(count)
+    x = int(data[:data.find('_')])
+    #print(x)
+    y = int(data[data.find('_')+1:-1])
+    #print(y)
+    if count == 1:
+        draw_cross(x,y)
+        count = 0
+    else:
+        draw_circle(x,y)
+        count = 1
+
+def restart(event):
+    main_canvas.delete('figure')
+    sock.sendto('restart'.encode(), dest)
+
+def main():
+    global dest
+    if conadr.get()=='localhost':
+        sock.bind(('0.0.0.0', 9090))
+        dest = ('255.255.255.255', 9090)
+        print('local is on')
+    else:
+        dest = (conadr.get(), 11719)
+        try:
+            sock.sendto(('Hello').encode(), dest)
+            print('Connection is on')
+        except:
+            print('Incorrect address')
+    main_canvas.delete('conn')
+    draw_field()
+    main_canvas.focus_set()
+    main_canvas.bind('<Button-1>', draw)
+    main_canvas.bind('<space>', restart)
+
+    def loopproc():
+        sock.setblocking(False)
+        try:
+            data = sock.recv(1024).decode()
+            #print(data)
+            if data=='restart':
+                main_canvas.delete('figure')
+            else:
+                draw_con(data)
+        except:
+            master.after(1,loopproc)
+            return
+        master.after(1,loopproc)
+        return
+    master.after(1,loopproc)
+
+conb = Button(main_canvas, text='Start', command=main)
+conb_window = main_canvas.create_window(200,400, window=conb, tag='conn')
 
 master.mainloop()
 
