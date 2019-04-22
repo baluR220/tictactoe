e0fdf88d91c2d7022f0c86f3b6d30f9b91c3a624
baluR220 <39237892+baluR220@users.noreply.github.com>
2019-04-22 16:22:12 +0700

    Update logic.py

--- temp/logic.py
+++ temp/logic.py
@@ -9,6 +9,7 @@ main_canvas.create_text(200, 50, text='Tic-Tac-Toe',
 count = 0
 free_mode = 0
 moves = [9 for i in range(9)]
+win_seq = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 normal_coords = [[0, 150, 0, 200, 100, 150, 0],
                  [150, 250, 0, 200, 200, 150, 1],
@@ -38,6 +39,26 @@ def watch_moves(x, y, form, cage):
         return(form)
     print(moves)
 
+def find_winner():
+    global moves, win_seq, count
+    winner = 'Draw.'
+    for line in win_seq:
+        if moves[line[0]] == moves[line[1]] == moves[line[2]] != 9:
+            if moves[line[0]] == 0:
+                winner = 'O is Winner!'
+            if moves[line[0]] == 1:
+                winner = 'X is Winner'
+            main_canvas.create_rectangle(100,150,300,250, fill='white', tag='figure')
+            main_canvas.create_text(200, 200, text=winner,
+                        justify=CENTER, font='Arial 24', tag='figure', fill='red')
+            for i in moves:
+                if i == 9:
+                    moves[moves.index(i)] = 2
+        if 9 not in moves and winner == 'Draw.':
+            main_canvas.create_text(200, 200, text=winner,
+                        justify=CENTER, font='Arial 24', tag='figure', fill='red')
+
+
 
 def draw(event):
     global count
@@ -46,6 +67,7 @@ def draw(event):
         x, y, cage = normalize(x, y)
         print(cage)
         last_form = watch_moves(x, y, count, cage)
+        find_winner()
     if count == 1:
         draw_cross(x, y)
         count = 0
@@ -81,7 +103,7 @@ def draw_circle(x, y):
 def restart(event):
     global count, moves
     count = 0
-    moves = [0 for i in range(8)]
+    moves = [9 for i in range(9)]
     main_canvas.delete('figure')
 
 
